cb074db1bbfe0b8f55d948a8503910f9277b8b32
Josh Ferriday <70819699+jferriday@users.noreply.github.com>
2021-02-13 16:01:06 +0000

    begin implementing cycle through banks function

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ async function getBanks(){
     return (await fetch(url)).json()
     }
 }
+
+
+
 // get the list of foodbanks and call updateHTML
 async function getBankList() {
     try{
@@ -21,10 +24,11 @@ async function getBankList() {
     }
 }
 
-function updateHTML(bankList){
+function updateHTML(bankList, bankIndex){
     // get the properties of the nearest food bank
     console.log(bankList);
-    const bank = bankList[0] // potentially offer ability to increment this to view banks further away
+    console.log('Bank index: ', bankIndex);
+    const bank = bankList[bankIndex] // potentially offer ability to increment this to view banks further away
     const name = bank.name;
     const distance = bank.distance_mi;
     const date = new Date(bank.needs.found);
@@ -52,15 +56,41 @@ function updateHTML(bankList){
 
 }
 
+// Functions to cycle forward or back through nearby food banks
+let bankIndex = 0; // initialise bank index to zero when page loads
+const incrementBank = () => {
+    bankIndex < 9 ? bankIndex +=1 : bankIndex = bankIndex;
+    updateHTML(bankIndex);
+}
 
-// prevent submission from refreshing the page:
+const decrementBank = () => {
+    bankIndex > 0 ? bankIndex -=1 : bankIndex = 0;
+    updateHTML(bankIndex);
+}
+
+
+
+
+// Buttons
+const submit = document.getElementById('submit');
+const previous = document.getElementById('previous');
+const nextButton = document.getElementById('next')
+
+// Event listener for submit button
 const searchForm = document.getElementById('search')
 searchForm.addEventListener('submit', (e) =>{
-    e.preventDefault();
+    e.preventDefault(); // prevent submission from refreshing the page:
+    bankIndex = 0 // reset bank index to zero for new search
     getBankList();
 })
 
 
-const submit = document.getElementById('submit');
+// Event listeners for next and previous buttons
+previous.addEventListener('click', decrementBank);
+
+nextButton.addEventListener('click', incrementBank);
+
+
+
 
 
